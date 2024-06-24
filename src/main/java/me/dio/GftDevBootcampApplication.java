a35cb118ac9240bed7a6fe2fcb86c16a1baf9ce7
Cilentoo <cilento762@gmail.com>
2024-06-24 08:34:32 -0300

    Initial commit

---
+++ src/main/java/me/dio/GftDevBootcampApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftDevBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GftDevBootcampApplication.class, args);
+	}
+
+}
